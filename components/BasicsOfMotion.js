92917bd1f026f494a144aa153e6c08b8e3e8e85b
Mateo <mateobonavia@gmail.com>
2024-06-26 11:29:33 -0300

    basics of framer motion

--- components/BasicsOfMotion.js
+++ components/BasicsOfMotion.js
@@ -3,6 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const BasicsOfMotion = () => {
   const [isVisible, setIsVisible] = useState(true);
+
   return (
     <div
       style={{
@@ -13,30 +14,36 @@ const BasicsOfMotion = () => {
       }}
     >
       <motion.button
-        onClick={() => setIsVisible(!isVisible)}
         className="example-button"
+        onClick={() => setIsVisible(!isVisible)}
         layout
       >
         Show/Hide
       </motion.button>
       <AnimatePresence mode="popLayout">
         {isVisible && (
+          // A cualquier elemento HTML se le puede agregar "motion." para poder hacer animaciones.
           <motion.div
+            // Le damos un valor inicial a la animación
             initial={{
               rotate: "0deg",
               scale: 0,
               y: 0,
             }}
+            // Le decimos que animación queremos que haga
             animate={{
               rotate: "180deg",
               scale: 1,
               y: [0, 150, -150, -150, 0],
             }}
+            // Le decimos que hacer cuando se desmonte el componente
             exit={{
               rotate: "0deg",
               scale: 0,
               y: 0,
             }}
+            // En transition podemos poner cosas como la duración, el easing, el tiempo que le damos
+            //  a cada valor y de la animación (expresado en porcentaje), entre otras cosas.
             transition={{
               duration: 1,
               ease: "backInOut",
