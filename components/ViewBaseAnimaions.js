b14a487f7582349f807e65ab0056c24299bf38a7
Mateo <mateobonavia@gmail.com>
2024-06-26 17:38:46 -0300

    whileInView | hook useInView

--- components/ViewBaseAnimaions.js
+++ components/ViewBaseAnimaions.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 const ViewBasedAnimations = () => {
-  return <></>;
+  // Creamos una referencia al div de la linea 19.
+  const ref = useRef(null);
+  // Con el hook useInView podemos saber si un componente está en la vista, por eso le pasamos la referencia al div.
+  // El segundo parámetro que le podemos pasar al useInView es un objeto con opciones
+  // en este caso le pasamos once: true para que solo se ejecute una vez.
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <>
+      <div style={{ height: "150vh" }} />
+      <motion.div
+        style={{ height: "100vh", background: "black" }}
+        initial={{ opacity: 0 }}
+        // Con el whileInView podemos darle animación a un componente cuando aparece en la vista.
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      />
+      <div
+        ref={ref}
+        style={{
+          height: "100vh",
+          background: isInView ? "blue" : "red",
+          transition: "1s background",
+        }}
+      />
+    </>
+  );
 };
 
 export default ViewBasedAnimations;
